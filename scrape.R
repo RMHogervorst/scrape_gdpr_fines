258c489933b7c3d77f4db093ec93bf6472e588ba
Roel M. Hogervorst <hogervorst.rm@gmail.com>
2020-04-09 11:57:18 +0200

    Update based on twitter conversation
    Using V8, based on @hrbrmstr and @IOrentus
    <https://twitter.com/hrbrmstr/status/1248058502884581376>

--- scrape.R
+++ scrape.R
@@ -1,27 +1,17 @@
 library(rvest)
-library(stringr)
+library(V8)
+ctx <- v8()
 link<- "https://www.privacyaffairs.com/gdpr-fines/"
 # some due dilligence. the robots.txt of this website allows all.
 # looking at the page source, It should be relatively simple to retrieve the data, it is
-# in the javascript session (see picture)
 page <- read_html(link)
 
 
-temp <- page %>% html_nodes("script") %>% .[9] %>% 
-  rvest::html_text() 
+page %>% html_nodes(xpath = "(.//script)[9]") %>% 
+  html_text() %>% 
+  ctx$eval()
 
-ends <- str_locate_all(temp, "\\]")
-starts <- str_locate_all(temp, "\\[")
-table1 <- temp %>% stringi::stri_sub(from = starts[[1]][1,2], to = ends[[1]][1,1]) %>% 
-  str_remove_all("\n") %>% 
-  str_remove_all("\r") %>%
-  jsonlite::fromJSON()
-
-##### I don't actually know what this is.
-# table2 <- temp %>% stringi::stri_sub(from = starts[[1]][2,2], to = ends[[1]][2,1]) %>% 
-#   str_remove_all("\n") %>% 
-#   str_remove_all("\r") %>%  
-#   jsonlite::fromJSON()
+table1 <- ctx$get("items")
 
 readr::write_csv(table1, "data/all_fines_raw.csv")
 
